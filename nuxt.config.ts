dd1c397f26cdf1c2f46128171e88beb9caae9b95
marijndegenaar <marijndegenaar@gmail.com>
2026-03-20 16:52:29 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,6 +28,8 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
       link: [
         { rel: 'canonical', href: 'https://andreabelosi.com' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'apple-touch-icon', href: '/favicon.png' },
       ],
     },
   },
